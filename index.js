538fac23a50f232c65013bd3a04e210a6e8deb19
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-04-28 12:13:09 +0600

    craftitems data send succussfully

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ app.get('/', (req, res) => {
   res.send('server is running')
 })
 
+
 // git config http.postBuffer 524288000
 
 
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.oypj9vn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // console.log(uri);
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -26,7 +28,9 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
+
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
@@ -34,33 +38,32 @@ async function run() {
     const craftsCollection = database.collection('crafts')
     // console.log(craftsCollection);
 
+    app.get('/mycrafts/:email',async(req,res)=>{
+      console.log(req.params); 
+      const result = await craftsCollection.find({email:req.params.email}).toArray();
+      res.send(result)
+    })
+
+    app.get('/crafts/:email',async(req,res)=>{
+      console.log(req.params); 
+      const result = await craftsCollection.find({email:req.params.email}).toArray();
+      res.send(result)
+    })
+ 
+    
+    
     app.get('/crafts/:id', async (req, res) => {
       let id = req.params.id
       const query = { _id: new ObjectId(id) }
       let result = await craftsCollection.findOne(query)
       res.send(result)
-      console.log(result);
+      // console.log(result);
     })
+   
+     
 
     // find many wise email 
 
-    // app.get('/crafts/:email',async(req,res)=>{
-    //   let email=req.params.email
-    //   let query={email:email}
-    //   // let result = craftsCollection.find(query)
-    //   // res.send(result)
-
-    //   let cursor= craftsCollection.find(query)
-    //   let result=await cursor.toArray()
-    //   res.send(result)
-    //   // console.log(email);
-    //   // let query= { email:  email  };
-    //   // let cursor =await craftsCollection.find(query)
-    //   // // let result =await cursor.toArray()
-    //   // res.send(cursor)
-    //   // console.log(cursor);
-
-    // })
 
     app.get('/crafts', async (req, res) => {
       let cursor = craftsCollection.find()
@@ -68,21 +71,30 @@ async function run() {
       res.send(result)
     })
 
-
-
-    app.post('/crafts', async (req, res) => {
-      const result = await craftsCollection.insertOne(req.body);
+    app.get('/allcraft', async (req, res) => {
+      let cursor = craftsCollection.find()
+      let result = await cursor.toArray()
       res.send(result)
     })
 
-    app.put('/crafts/:id', async(req, res) => {
+    
+    app.post('/craftitems', async (req, res) => {
+      const result = await craftsCollection.insertOne(req.body);
+      res.send(result)
+      console.log(result);
+    })
+
+
+    
+  
+
+    app.put('/update/:id', async(req, res) => {
       let item = req.body
       let id = req.params.id
       let filter = { _id: new ObjectId(id) }
       let options = { upsert: true }
       let updatedItem = {
         $set: {
-
               image:item.image,
               item_name:item.item_name,
               subcategory_Name:item.subcategory_Name,
@@ -97,8 +109,21 @@ async function run() {
       res.send(result)
     })
 
+    
 
-    // Send a ping to confirm a successful connection
+
+
+    app.delete('/crafts/:id',async(req,res)=>{
+      let id=req.params.id 
+      let item=req.body
+      let query= {_id:new ObjectId(id)}
+      let result= await craftsCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
+       
+  // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
